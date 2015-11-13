5fd7a1c98ab01fa5d3999304884f69eac1a7bbc3
Martin Møller <mmj@dbc.dk>
2015-11-13 13:57:27 +0100

    Re-enabled Kafka. sd-149

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -2,8 +2,8 @@
 
 import Winston from 'winston';
 import expressWinston from 'express-winston';
-import {Syslog} from 'winston-syslog'; // eslint-disable-line no-unused-vars
-// import kafkaTransport from 'winston-kafka-transport'; // eslint-disable-line
+import {Syslog} from 'winston-syslog'; // eslint-disable-line
+import kafkaTransport from 'winston-kafka-transport';
 import os from 'os';
 
 const PRODUCTION = (process.env.NODE_ENV === 'production'); // eslint-disable-line
@@ -72,7 +72,7 @@ function getTransports(config) {
       handleExceptions: true
     })
   ];
-/*
+
   if (KAFKA_TOPIC && KAFKA_HOST) {
     Winston.transports.Kafka = kafkaTransport;
     const kafka = new Winston.transports.Kafka({
@@ -83,7 +83,7 @@ function getTransports(config) {
 
     transports.push(kafka);
   }
-*/
+
   return transports;
 }
 
